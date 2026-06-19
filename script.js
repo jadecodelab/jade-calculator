4b933f637a05f021ef4993053fd22eb38dd5b004
Jade <ch.jade427@gmail.com>
2026-06-18 20:00:48 -0700

    Create calculator buttons and basic operator functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return "Can't divide by 0!";
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  if (operator === "+") return add(a, b);
+  else if (operator === "-") return substract(a, b);
+  else if (operator === "*") return multiply(a, b);
+  else if (operator === "/") return divide(a, b);
+  else return "Invalid operator!";
+}
