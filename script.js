ab3401df8c931c6f66494b94b584ff9c125edf48
Jade <ch.jade427@gmail.com>
2026-06-18 20:34:30 -0700

    Enable number entry and operator selection

--- script.js
+++ script.js
@@ -22,3 +22,40 @@ function operate(operator, a, b) {
   else if (operator === "/") return divide(a, b);
   else return "Invalid operator!";
 }
+
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+const display = document.querySelector(".display");
+
+function appendNumber(num) {
+  if (operator === "") {
+    firstNum += num;
+    display.textContent = firstNum;
+  } else {
+    secondNum += num;
+    display.textContent = secondNum;
+  }
+
+  console.log(firstNum, operator, secondNum);
+}
+
+function setOperator(op) {
+  operator = op;
+}
+
+const numberButtons = document.querySelectorAll(".numbers button");
+const operatorButtons = document.querySelectorAll(".operators button");
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    appendNumber(button.textContent);
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    operator = button.textContent;
+    display.textContent = operator;
+  });
+});
